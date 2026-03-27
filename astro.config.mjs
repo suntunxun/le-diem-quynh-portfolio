a3a6150dcef7d5ac6690fc6fa71ad051a19a1b1d
phamhuutri108 <phamhuutri108@gmail.com>
2026-03-27 10:58:34 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 
+import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
+import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
+import keystatic from '@keystatic/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://lediemquynh.com',
-	integrations: [mdx(), sitemap()],
+	output: 'server',
+	adapter: cloudflare({ imageService: 'compile' }),
+	integrations: [mdx(), react(), sitemap(), keystatic()],
 });
