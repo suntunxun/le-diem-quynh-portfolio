aa923faaf5361049374867daedd2df53da3a4938
Tri Pham <phamhuutri108@gmail.com>
2026-04-14 23:22:46 +0700

    Migrate CMS to CloudCannon and clean up Keystatic/Netlify Identity

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import cloudflare from '@astrojs/cloudflare';
 import mdx from '@astrojs/mdx';
 import react from '@astrojs/react';
 import sitemap from '@astrojs/sitemap';
-import keystatic from '@keystatic/astro';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
@@ -12,5 +11,6 @@ export default defineConfig({
 	site: 'https://lediemquynh.com',
 	output: 'server',
 	adapter: cloudflare({ imageService: 'compile' }),
-	integrations: [mdx(), react(), sitemap(), keystatic()],
+	integrations: [mdx(), react(), sitemap()],
 });
+
